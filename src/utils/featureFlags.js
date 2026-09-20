12b793cb3d20c1109243151d1c44f2175f4c2058
Your Name <you@example.com>
2026-09-20 18:50:11 +0530

    Update font weights for headings

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -37,3 +37,4 @@ export const flags = {};
 // Feature tweak: update_success_page_illustration - 1789910398996
 // Feature tweak: add_empty_state_to_results - 1789910402960
 // Feature tweak: enhance_category_search_filter - 1789910406926
+// Feature tweak: update_font_weights_for_headings - 1789910410758
