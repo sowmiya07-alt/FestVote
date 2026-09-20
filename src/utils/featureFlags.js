bb5c0405a90197775bc5b2dc4f848b633b150776
Your Name <you@example.com>
2026-09-20 18:37:11 +0530

    Fix typo in landing page

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -4,3 +4,4 @@ export const flags = {};
 // Feature tweak: refactor_navbar_spacing - 1789909606870
 // Feature tweak: update_hero_section_colors - 1789909611367
 // Feature tweak: add_hover_effects_to_buttons - 1789909615345
+// Feature tweak: fix_typo_in_landing_page - 1789909631154
