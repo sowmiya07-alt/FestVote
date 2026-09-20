9a9145e03b02a41eb70705f784866bd419980166
Your Name <you@example.com>
2026-09-20 18:37:23 +0530

    Optimize image loading

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -6,3 +6,4 @@ export const flags = {};
 // Feature tweak: add_hover_effects_to_buttons - 1789909615345
 // Feature tweak: fix_typo_in_landing_page - 1789909631154
 // Feature tweak: improve_responsive_layout_for_mobile - 1789909636825
+// Feature tweak: optimize_image_loading - 1789909643565
