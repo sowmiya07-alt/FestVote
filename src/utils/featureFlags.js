0dcc3778e30b40e31897e00429319fd975d47adb
Sowmiya S <sowmiyasuresh71@gmail.com>
2026-09-20 18:57:21 +0530

    Improve semantic markup in layout

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -73,3 +73,4 @@ export const flags = {};
 // UI/UX Improvement: add_scroll_to_top_behavior_on_route_change - 1789910829000
 // UI/UX Improvement: update_success_animation_duration - 1789910832687
 // UI/UX Improvement: add_empty_state_for_voted_categories - 1789910836409
+// UI/UX Improvement: improve_semantic_markup_in_layout - 1789910840651
