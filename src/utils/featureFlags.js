9df5fbc35abce3b130b8c9e629c6b4e85e00d347
Sowmiya S <sowmiyasuresh71@gmail.com>
2026-09-20 18:58:39 +0530

    Refine admin dashboard grid

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -91,3 +91,4 @@ export const flags = {};
 // UI/UX Improvement: improve_tab_navigation_accessibility - 1789910906680
 // UI/UX Improvement: update_layout_max-width - 1789910910739
 // UI/UX Improvement: add_custom_selection_styling - 1789910914853
+// UI/UX Improvement: refine_admin_dashboard_grid - 1789910919132
