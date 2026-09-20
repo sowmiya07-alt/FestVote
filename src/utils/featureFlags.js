b3a723c865e10ae7f513f7418673d87710f6e2b0
Sowmiya S <sowmiyasuresh71@gmail.com>
2026-09-20 18:55:52 +0530

    Optimize bundle size with code splitting

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -56,3 +56,4 @@ export const flags = {};
 // UI/UX Improvement: update_favicon_to_festvote_logo - 1789910725639
 // UI/UX Improvement: add_transition_delays_to_cards - 1789910730417
 // UI/UX Improvement: fix_margin_on_dashboard_stats - 1789910738107
+// UI/UX Improvement: optimize_bundle_size_with_code_splitting - 1789910752448
