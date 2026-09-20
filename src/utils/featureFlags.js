cb9bdd8cb4a3f787d0ce56375fe0001fbabcffa6
Sowmiya S <sowmiyasuresh71@gmail.com>
2026-09-20 18:56:40 +0530

    Update input borders on focus

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -64,3 +64,4 @@ export const flags = {};
 // UI/UX Improvement: update_mock_data_for_categories - 1789910786242
 // UI/UX Improvement: add_subtle_background_pattern - 1789910790562
 // UI/UX Improvement: fix_text_alignment_on_mobile - 1789910795304
+// UI/UX Improvement: update_input_borders_on_focus - 1789910799662
