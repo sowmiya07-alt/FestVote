2155d7d05ae6d602a211a9a906e2d79af1dd3a13
Sowmiya S <sowmiyasuresh71@gmail.com>
2026-09-20 18:57:13 +0530

    Update success animation duration

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -71,3 +71,4 @@ export const flags = {};
 // UI/UX Improvement: enhance_profile_avatar_styling - 1789910817852
 // UI/UX Improvement: fix_z-index_context_on_navigation - 1789910823700
 // UI/UX Improvement: add_scroll_to_top_behavior_on_route_change - 1789910829000
+// UI/UX Improvement: update_success_animation_duration - 1789910832687
