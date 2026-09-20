8e4743819878adbd0c1d5c126e130279bee3cbbb
Sowmiya S <sowmiyasuresh71@gmail.com>
2026-09-20 18:55:30 +0530

    Add transition delays to cards

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -54,3 +54,4 @@ export const flags = {};
 // UI/UX Improvement: add_responsive_breakpoints_for_tablet - 1789910718279
 // UI/UX Improvement: improve_contrast_ratio_on_text - 1789910722016
 // UI/UX Improvement: update_favicon_to_festvote_logo - 1789910725639
+// UI/UX Improvement: add_transition_delays_to_cards - 1789910730417
