23d86497efc91d921fc01c48ce5b41dc65f907b0
Sowmiya S <sowmiyasuresh71@gmail.com>
2026-09-20 18:56:06 +0530

    Update voting progress bar color

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -58,3 +58,4 @@ export const flags = {};
 // UI/UX Improvement: fix_margin_on_dashboard_stats - 1789910738107
 // UI/UX Improvement: optimize_bundle_size_with_code_splitting - 1789910752448
 // UI/UX Improvement: add_custom_error_404_page_placeholder - 1789910761152
+// UI/UX Improvement: update_voting_progress_bar_color - 1789910765931
