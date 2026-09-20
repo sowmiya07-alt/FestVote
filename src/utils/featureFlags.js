f3eff46825ed988ea220f43d50d6360831ace27e
Sowmiya S <sowmiyasuresh71@gmail.com>
2026-09-20 18:58:18 +0530

    Update landing page copy

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -86,3 +86,4 @@ export const flags = {};
 // UI/UX Improvement: update_result_progress_bar_styling - 1789910885012
 // UI/UX Improvement: add_clear_selection_button - 1789910889692
 // UI/UX Improvement: fix_flex_wrap_on_filter_buttons - 1789910893649
+// UI/UX Improvement: update_landing_page_copy - 1789910897841
