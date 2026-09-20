a2c05be7e77088e58226bcf80b4a1f8a5ee3a61b
Your Name <you@example.com>
2026-09-20 18:50:37 +0530

    Add subtle pulse animation

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -42,3 +42,4 @@ export const flags = {};
 // Feature tweak: refactor_voting_logic_hook - 1789910425790
 // Feature tweak: improve_toast_notification_ui - 1789910429892
 // Feature tweak: update_login_page_background - 1789910433562
+// Feature tweak: add_subtle_pulse_animation - 1789910437150
